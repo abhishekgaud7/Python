59ed43431eaa67585486905705a3b56dc300d48e
Abhishek Gaud <romanreings59368@gmail.com>
2025-12-20 12:50:29 +0530

    update

---
+++ Practice/Ques4.py
@@ -0,0 +1,19 @@
+#  IN these code ill using hashtag to show the changes made recently\
+
+print("This is Ques4 file")
+#  so that you can compare with other files easily.
+x=5
+y=10
+z=x*y
+print(z)
+ 
+#  now add some text here to show changes made recently.
+#  This is the end of the changes made recently.
+#  You can compare with other files now.
+#  Compare this snippet from Practice/Ques3.py:
+# a=3
+# b=4   
+# c=a+b
+# print(c)
+#  Compare this snippet from Practice/Ques1.py:
+# print("Hello World")  
