e9a6f3e7eed1fa1dee1330efc4772b113223e8f2
Abhishek Gaud <romanreings59368@gmail.com>
2025-12-20 21:19:49 +0530

    update

---
+++ Practice2/Ques1.py
@@ -0,0 +1,8 @@
+age = 21
+name = "Abhishek"
+mobile_No = 9171214043
+CGPA = 7.5
+print("Hello ,", name)
+print("Your age is :", age)
+print("Your mobile number is :", mobile_No)
+print("Your CGPA is :", CGPA)
---
+++ Practice2/Ques3.py
@@ -0,0 +1,15 @@
+age = 21
+print("Your age is : ",age)
+print(type(age))
+
+cgpa = 7.5
+print("Your CGPA is : ",cgpa)
+print(type(cgpa))
+
+name = "Abhishek"
+print("Your name is : ",name)
+print(type(name))
+
+is_completed = True
+print("Is the course completed ? : ",is_completed)
+print(type(is_completed))
