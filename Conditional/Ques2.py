c6785483d6b78b878e1007cf667e5d6dd0fcbfc0
Abhishek Gaud <romanreings59368@gmail.com>
2025-12-22 16:26:21 +0530

    commit

---
+++ Conditional/Ques2.py
@@ -0,0 +1,8 @@
+age = 121
+
+
+if(age>18):
+    print("You are eligible to vote")
+    print("Thank you ")
+
+print("This line is always executed")    
---
+++ Conditional/Ques4.py
@@ -0,0 +1,9 @@
+age = int(input("Enter your age:"))
+
+if(age>18):
+    print("You are eligible to vote")
+
+elif(age==18):
+    print("You are eligible to vote this year")
+else:
+    print("You are not eligible to vote")   
