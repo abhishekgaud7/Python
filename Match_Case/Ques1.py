ec21dbb39b2563bfe944ab71ac227d47cd11074b
Abhishek Gaud <romanreings59368@gmail.com>
2025-12-22 16:50:26 +0530

    update

---
+++ Match_Case/Ques1.py
@@ -0,0 +1,23 @@
+a=int(input("Enter the number between 1 To 100:"))
+
+
+match a:
+    case 19:
+        print("You Won a car")
+    case 24:
+        print("You Won a bike")
+    case 63:
+        print("You Won a rolex")
+    case 14:
+        print("You Won a charger")
+    case 56:
+        print("You Won a phone")
+    case 79:
+        print("You Won a laptop")
+    case 49:
+        print("You Won a tv")
+    case 98:
+        print("You Won a cooler")
+    case _:
+        print("Better luck next time")
+        
