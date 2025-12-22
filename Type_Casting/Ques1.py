85f71ed30a0ac8c124e8507564e65fce70796667
Abhishek Gaud <romanreings59368@gmail.com>
2025-12-22 10:32:03 +0530

    Create Ques1.py

---
+++ Type_Casting/Ques1.py
@@ -0,0 +1,26 @@
+d=234
+a=45
+print(a)
+print(type(a))
+
+b="34"
+print(b)
+print(type(b))
+
+#casting b to an interger
+
+c=int(b)
+print(c)
+print(type(c))
+
+print(d)
+print(type(d))
+
+e=str(d)
+print(e)
+print(type(e))
+
+pi=3.14
+pi_int=int(pi)
+print(pi_int)
+print(type(pi_int))
