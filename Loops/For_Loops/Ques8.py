880085a66f8a5b5e8c0366a3658674d86fe0d762
Abhishek Gaud <romanreings59368@gmail.com>
2026-01-01 20:20:37 +0530

    Update Ques8.py

--- Loops/For_Loops/Ques8.py
+++ Loops/For_Loops/Ques8.py
@@ -3,3 +3,4 @@
 # Then, it should print the first n even natural numbers.
 # For example, if the user enters 5, the program should print 2, 4, 6, 8, 10.
 # The program should also calculate and print the sum of these even natural numbers.
+# Finally, the program should display the average of these even natural numbers.
