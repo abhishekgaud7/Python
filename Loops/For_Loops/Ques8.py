ea554c315cc34577116720632af5902390611425
Abhishek Gaud <romanreings59368@gmail.com>
2026-01-01 20:20:09 +0530

    Update Ques8.py

--- Loops/For_Loops/Ques8.py
+++ Loops/For_Loops/Ques8.py
@@ -1,4 +1,4 @@
 # write a program to print the first n even natural numbers using a for loop
 # The program should ask the user to enter the value of n.
 # Then, it should print the first n even natural numbers.
-#
+# For example, if the user enters 5, the program should print 2, 4, 6, 8, 10.
