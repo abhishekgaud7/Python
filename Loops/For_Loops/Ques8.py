4d8f68616a6d4cb15260e979148daf2aa6743ca3
Abhishek Gaud <romanreings59368@gmail.com>
2026-01-02 13:31:00 +0530

    Update Ques8.py

--- Loops/For_Loops/Ques8.py
+++ Loops/For_Loops/Ques8.py
@@ -1,1 +1,27 @@
-g
+#write a program for making a single page webapp using flask
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('index.html')
+if __name__ == '__main__':
+    app.run(debug=True)
+# The above code creates a simple Flask web application that serves a single page.
+# You need to create an 'index.html' file in a folder named 'templates' in the same directory as this script.
+# The 'index.html' file can contain any HTML content you want to display on the web page.
+# Example content for 'index.html':
+'''
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Single Page Webapp</title>
+</head>
+<body>
+    <h1>Welcome to My Webapp!</h1>
+    <p>This is a simple single page web application using Flask.</p>
+</body>
+</html>
+'''
+# Save the above HTML content in 'templates/index.html' to see it rendered by the Flask app.
+
+    
