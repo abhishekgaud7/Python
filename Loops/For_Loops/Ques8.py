278461954a594a2796131da5e948924253491ca3
Abhishek Gaud <romanreings59368@gmail.com>
2026-01-05 18:30:22 +0530

    Update Ques8.py

--- Loops/For_Loops/Ques8.py
+++
@@ -1,7 +0,0 @@
-# write 
-# a program to print the first n even natural numbers using for loop
-
-# taking input from user
-# n represents how many even natural numbers to print
-# we convert the input to integer as input() function returns string
-# we use int() function to convert string to integer
