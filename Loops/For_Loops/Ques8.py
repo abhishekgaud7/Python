8b168f306317ea65dcaf693ca64e2e23bed8a416
Abhishek Gaud <romanreings59368@gmail.com>
2026-01-07 11:55:01 +0530

    Update Ques8.py

--- Loops/For_Loops/Ques8.py
+++ Loops/For_Loops/Ques8.py
@@ -1,5 +1,14 @@
 # write a program to build a libraary management system using for loop.
 # user inputs book names to add to the library until they decide to stop.
 # After adding books, display the list of all books in the library.
+library = []
+while True:
+    book_name = input("Enter the name of the book to add to the library (or type 'stop' to finish): ")
+    if book_name.lower() == 'stop':
+        break
+    library.append(book_name)
+print("\nBooks in the library:")
+for book in library:
+    print("- " + book)
 
-
+                
