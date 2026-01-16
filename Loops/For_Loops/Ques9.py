338bf430cb5f6f7592488285d9f6f46ef91d8f63
Abhishek Gaud <romanreings59368@gmail.com>
2026-01-16 12:17:43 +0530

    Update Ques9.py

--- Loops/For_Loops/Ques9.py
+++
@@ -1,7 +0,0 @@
-# write a program to print the first n even natural numbers using a for loop
-# taking input from the user
-
-# Function to print first n even natural numbers
-# n: number of even natural numbers to print
-# returns: None
-def print_even_natural_numbers(n):
