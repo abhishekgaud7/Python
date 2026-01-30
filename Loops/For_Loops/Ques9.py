5822bf290d751487e407eb0e8456a7efc0007f01
Abhishek Gaud <romanreings59368@gmail.com>
2026-01-30 15:24:47 +0530

    Update Ques9.py

--- Loops/For_Loops/Ques9.py
+++ Loops/For_Loops/Ques9.py
@@ -1,2 +1,2 @@
 #write a pro
-#how to print the multiplication table of a given number using for loopgram to print the multiplication table of a given number using for loop
+#how to print the multiplication table of a given number using for loopgram to print the multiplication table of a given number using for loop2
