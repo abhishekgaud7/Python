7f0a649dd2aa545b6f82f01c5a01b19768be645a
Abhishek Gaud <romanreings59368@gmail.com>
2026-02-05 20:41:42 +0530

    Update Ques9.py

--- Loops/For_Loops/Ques9.py
+++ Loops/For_Loops/Ques9.py
@@ -4,3 +4,4 @@
 #how many times the word "the" is present in the above string
 # write a program to count the number of times the word "the" is present in the above string
 # the_string = "djbwufgu bduinjbjuds the write"
+# count = 0
