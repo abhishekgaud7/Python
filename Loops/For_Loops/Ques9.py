fd9fe40996c759b0dace4d9bf7e8a00ae5d31833
Abhishek Gaud <romanreings59368@gmail.com>
2026-02-09 10:39:52 +0530

    Update Ques9.py

--- Loops/For_Loops/Ques9.py
+++ Loops/For_Loops/Ques9.py
@@ -1,2 +1,3 @@
 # banking system program
 # 1. create a class called BankAccount with attributes like account_number, account_holder_name
+# 2. create methods for deposit, withdraw, and display_balance
